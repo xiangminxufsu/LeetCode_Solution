44f922b985a66ffd01da6d7a00b0db901f31459c
xiangmin xu <geo.xuxiangmin@gmail.com>
2014-08-08 03:29:48 +0000

    Merge Sorted Array.cpp created online with Bitbucket

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        while(n>0){
+            if(m>0&&A[m-1]>B[n-1]){
+                A[m+n-1]=A[m-1];
+                m--;
+            }
+            else{
+                A[m+n-1]=B[n-1];
+                n--;
+            }
+        }
+    }
+};
